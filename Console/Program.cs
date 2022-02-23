92f874e228cbea7eebf8d92caa82397db66aec37
Mads Tinko Madsen <mtm@itklyngen.dk>
2022-02-23 23:53:48 +0100

    More attempts at equals. (update) to not try to add object if it exists.

--- Console/Program.cs
+++ Console/Program.cs
@@ -9,6 +9,40 @@ var airlines = context.Airlines.ToList();
 var airports = context.Airports.ToList();
 var gates = context.Gates.ToList();
 
+Booking bookingCreate = new Booking();
+bookingCreate.Time = new DateTime(2022, 02, 23, 08, 15, 00);
+bookingCreate.Airline = new Airline() { AirlineName = "ALSIE EXPRESS A/S" };
+bookingCreate.Airport = new Airport() { AirportName = "Sonderborg Airport", City = "Sonderborg", Country = "Denmark" };
+bookingCreate.FlightNumber = "6I102";
+bookingCreate.Gate = new Gate() { GateName = "A9"};
+//Trying to get a check on if it exists and then update it instead.
+
+//foreach (var booking in bookings)
+//{
+//    if (booking.BookingId == bookingCreate.BookingId)
+//    {
+//        context.Bookings.Update(bookingCreate);
+//    }
+//    else
+//    {
+//        context.Bookings.Add(bookingCreate);
+//    }
+//}
+
+//    if (bookings.Contains(bookingCreate))
+//{
+//    context.Bookings.Update(bookingCreate);
+//}
+//else
+//{
+//    context.Bookings.Add(bookingCreate);
+//}
+
+context.Bookings.Add(bookingCreate);
+context.SaveChanges();
+
+
+//Prints our bookings.
 foreach (var booking in bookings)
 {
     //Console.WriteLine("Booking id: " + booking.BookingId);
@@ -30,13 +64,5 @@ foreach (var booking in bookings)
     Console.WriteLine("");
 }
 
-Booking bookingCreate = new Booking();
-bookingCreate.Time = new DateTime(2022, 02, 23, 08, 15, 00);
-bookingCreate.Airline = new Airline() { AirlineName = "ALSIE EXPRESS A/S" };
-bookingCreate.Airport = new Airport() { AirportName = "Sonderborg Airport", City = "Sonderborg", Country = "Denmark" };
-bookingCreate.FlightNumber = "6I102";
-bookingCreate.Gate = new Gate() { GateName = "A9"};
-context.Bookings.Add(bookingCreate);
-context.SaveChanges();
 
 Console.WriteLine("Hello, World!");
