a07945c11b88d07cceec55906e5ebd5de31fc4ec
Mads Tinko Madsen <50380211+T1NK0@users.noreply.github.com>
2022-02-23 23:57:32 +0100

    Delete Test directory

--- Test/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using CopenhagenAirport.Data.Models;
-
-Console.WriteLine("Hello, World!");
-
-CopenhagenAirportContext context = new CopenhagenAirportContext();
-
-
